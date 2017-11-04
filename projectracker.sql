3dda7a227b9c19baa5ac213b96f247fb9895c0da
mushu <showndarya.m@somaiya.edu>
2017-11-04 17:23:54 +0530

    Updated database SQL file

--- projectracker.sql
+++ projectracker.sql
@@ -40,7 +40,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`ad_id`, `ad_un`, `ad_name`, `p_id`) VALUES
-(2, '	 domain_admin1', 'Admin', 1);
+(2, '	 domain_admin1', 'Admin', 1),
+(4, 'domain_admin2', 'admin2', 2);
 
 -- --------------------------------------------------------
 
@@ -60,7 +61,8 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` (`emp_id`, `emp_username`, `emp_name`, `emp_dept`) VALUES
-(1, 'domain_user1', 'User', 'Dept');
+(1, 'domain_user1', 'User', 'Dept'),
+(3, 'domain_user2', 'Mushu', 'Dept');
 
 -- --------------------------------------------------------
 
@@ -83,7 +85,9 @@ INSERT INTO `empro` (`sn`, `emp_id`, `p_id`) VALUES
 (2, 1, 2),
 (3, 1, 3),
 (4, 1, 4),
-(5, 1, 5);
+(5, 1, 5),
+(8, 3, 1),
+(9, 3, 2);
 
 -- --------------------------------------------------------
 
@@ -99,18 +103,19 @@ CREATE TABLE `issue` (
   `issue_desc` varchar(200) DEFAULT NULL,
   `cat_id` int(20) DEFAULT NULL,
   `status` varchar(50) NOT NULL DEFAULT 'Open',
-  `assign` int(11) NOT NULL
+  `assign` int(11) NOT NULL,
+  `assign_to` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `issue`
 --
 
-INSERT INTO `issue` (`issue_id`, `emp_id`, `project_id`, `issue_name`, `issue_desc`, `cat_id`, `status`, `assign`) VALUES
-(1, 1, 1, 'issue1', 'this is an issue', 1, 'Closed', 1),
-(2, 1, 1, 'issue2', 'this is an issue2', 3, 'In Progress', 0),
-(9, 1, 1, 'issue3', 'this is an issue3', 5, 'Open', 1),
-(10, 1, 2, 'issue4', 'First issue in project 2', 0, 'Open', 0);
+INSERT INTO `issue` (`issue_id`, `emp_id`, `project_id`, `issue_name`, `issue_desc`, `cat_id`, `status`, `assign`, `assign_to`) VALUES
+(1, 1, 1, 'issue1', 'this is an issue', 1, 'Closed', 1, 1),
+(2, 1, 1, 'issue2', 'this is an issue2', 3, 'In Progress', 0, 0),
+(9, 1, 1, 'issue3', 'this is an issue3', 5, 'Open', 1, 1),
+(10, 1, 2, 'issue4', 'First issue in project 2', 0, 'In Progress', 1, 3);
 
 -- --------------------------------------------------------
 
@@ -154,7 +159,9 @@ CREATE TABLE `login` (
 
 INSERT INTO `login` (`id`, `username`, `pwd`, `admin`) VALUES
 (1, 'domain_user1', 'user1', 0),
-(2, 'domain_admin1', 'admin1', 1);
+(2, 'domain_admin1', 'admin1', 1),
+(3, 'domain_user2', 'user2', 0),
+(4, 'domain_admin2', 'admin2', 1);
 
 -- --------------------------------------------------------
 
@@ -179,7 +186,7 @@ CREATE TABLE `project` (
 
 INSERT INTO `project` (`p_id`, `p_name`, `p_desc`, `p_sd`, `p_ed`, `p_location`, `p_budget`, `p_client`) VALUES
 (1, 'PROJ1', 'The project is P1. The admin of the project is domain_admin1.This should work.', '12/10/2017', '1/11/2017', 'Mumbai', 1200, 'Client1'),
-(2, 'PROJ2', 'The project is P2. The admin of the project is domain_admin1.', '01/01/2017', '03/03/2017', 'Mumbai', 10000, 'Client2'),
+(2, 'PROJ2', 'The project is P2. The admin of the project is domain_admin2.', '01/01/2017', '03/03/2017', 'Mumbai', 10000, 'Client2'),
 (3, 'PROJ3', 'The project is P3. The admin of the project is domain_admin1.', '11/04/2017', '30/05/2017', 'Mumbai', 20000, 'Client3'),
 (4, 'PROJ4', 'The project is P4. The admin of the project is domain_admin1.', '21/03/2017', '15/05/2017', 'Mumbai', 30000, 'Client4'),
 (5, 'PROJ5', 'The project is P5. The admin of the project is domain_admin1.', '18/07/2017', 'Present', 'Mumbai', 40000, 'Client5');
@@ -244,17 +251,17 @@ ALTER TABLE `project`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `ad_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ad_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `emp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `emp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `empro`
 --
 ALTER TABLE `empro`
-  MODIFY `sn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `sn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `issue`
 --
@@ -269,7 +276,7 @@ ALTER TABLE `issuecat`
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `project`
 --
