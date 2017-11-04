998f3ff49ac331028fdfc50f44f0b8dca2359f15
mushu <showndarya.m@somaiya.edu>
2017-11-04 15:40:08 +0530

    More fields db update in Admin manage

--- update_issue.php
+++ update_issue.php
@@ -14,8 +14,13 @@
 		$pid = $_REQUEST["pn"];
 		$pname = $_REQUEST["pname"];
 		$pdesc = $_REQUEST["pdesc"];
-		
-		$sql = "UPDATE project SET p_name='$pname', p_desc = '$pdesc' WHERE p_id='$pid'";
+		$psd = $_REQUEST["psd"];
+		$ped = $_REQUEST["ped"];
+		$ploc = $_REQUEST["ploc"];
+		$pbud = $_REQUEST["pbud"];
+		$pcli = $_REQUEST["pcli"];
+
+		$sql = "UPDATE project SET p_name='$pname', p_desc = '$pdesc', p_sd = '$psd', p_ed = '$ped', p_location = '$ploc', p_budget = '$pbud', p_client = '$pcli' WHERE p_id='$pid'";
 		if(mysqli_query($con,$sql))
 		{
 			session_start();
