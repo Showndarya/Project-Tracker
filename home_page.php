e8f4e21bc3819278c68c4fdefd2ca4dc9cf5780e
mushu <showndarya.m@somaiya.edu>
2017-11-05 02:28:43 +0530

    Removed home_page.js

--- home_page.php
+++ home_page.php
@@ -246,5 +246,44 @@ vertical-align:middle;
           <b>Client:</b> <p id="pcli"></p>
     </div>
 </div>
+
+<script type="text/javascript">
+  
+  $('.button').on('click', {
+    'name': function(element) {
+      return $(element).data('name');
+    },
+    'desc': function(element) {
+      return $(element).data('desc');      
+    },
+    'sd': function(element) {
+      return $(element).data('sd');      
+    },
+    'ed': function(element) {
+      return $(element).data('ed');      
+    },
+    'loc': function(element) {
+      return $(element).data('loc');      
+    },
+    'bud': function(element) {
+      return $(element).data('bud');      
+    },
+    'cli': function(element) {
+      return $(element).data('cli');      
+    },
+
+  },
+
+  function(e) {
+    $('#pname').html(e.data.name(this));
+    $('#pdata').html(e.data.desc(this));
+    $('#psd').html(e.data.sd(this));
+    $('#ped').html(e.data.ed(this));
+    $('#ploc').html(e.data.loc(this));
+    $('#pbud').html(e.data.bud(this));
+    $('#pcli').html(e.data.cli(this));
+  });
+
+</script>
 </body>
 </html>
